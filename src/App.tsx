b0196d493ed3f5571fec979f2ff0b9673cece10e
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2024-03-06 19:05:41 -0300

    thingd that wasnt using

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import React,{useState} from 'react';
+import {useState} from 'react';
 import { fetchCharacter } from './script'
 
 interface CharacterData{
